decb00d7f74594f9674da7178abab4083f32faae
redbeanv <nan_hongbin@naver.com>
2021-01-06 17:11:19 +0900

    add basic react-next layout

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const About = () => (
+  <AppLayout>
+    <h1>소개페이지</h1>
+  </AppLayout>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const Main = () => (
+  <AppLayout>
+    <h1>The Legend of Zelda</h1>
+    <h2>Breath of the Wild</h2>
+  </AppLayout>
+);
+
+export default Main;
