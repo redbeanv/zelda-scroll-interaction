dca315be073e27ded0cecfe81405a2b097cb85f1
owopqowo <54467533+owopqowo@users.noreply.github.com>
2021-01-08 10:25:26 +0900

    add about

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import Link from 'next/link';
 import AppLayout from '../../components/AppLayout';
-import Head from 'next';
 import Header from '../../components/header/header';
+import Profile from '../../components/profile/profile';
+import styles from './about.module.css';
 
-const About = () => (
-  <AppLayout>
-    <Header />
-    <h1>소개페이지</h1>
-  </AppLayout>
-);
+const About = () => {
+  const isAbout = true;
+  const profiles = [
+    {
+      name: 'kimhb',
+      message: 'developer',
+      img: 'https://placeimg.com/300/300/animals',
+      sns: [
+        {
+          name: 'github',
+          url: 'www'
+        }
+      ]
+    },
+    {
+      name: 'wonij',
+      message: 'publiser',
+      img: 'https://placeimg.com/300/300/any',
+      sns: [
+        {
+          name: 'github',
+          url: 'www'
+        },
+        {
+          name: 'facebook',
+          url: 'www'
+        }
+      ]
+    },
+  ]
+  return (
+    <AppLayout>
+      <div className={styles.about}>
+        <Header isAbout={isAbout} />
+        <ul className={styles.list}>
+        {profiles.map((profile, index) => (
+          <Profile key={index} profile={profile} />
+        ))}
+        </ul>
+      </div>
+    </AppLayout>
+  )
+};
 
 export default About;
