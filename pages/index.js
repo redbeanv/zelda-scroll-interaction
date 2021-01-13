9f8a3b3d0586d150e3a328a6573ca6ad46d0f750
redbeanv <nan_hongbin@naver.com>
2021-01-13 23:46:15 +0900

    fix scroll event & feat stickSection component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import AppLayout from '../components/appLayout';
 import Gallery from '../components/gallery';
+import StickySection from "../components/main/stickySection";
 import styles from '../public/css/pages/index.module.css';
 import path from 'path';
 import fs from 'fs';
@@ -11,16 +12,16 @@ const VIDEO_PATH = '/images/video';
 const HEIGHT_NUM = 5;
 
 const Main = ({sceneInfoList}) => {
-  const [sceneInfos, setSceneInfos] = useState([]);
+  const [sceneInfos, setSceneInfos] = useState([]); // scene object list
   const [layoutData, setLayoutData] = useState({
-    yOffset: 0,           // window.pageYOffset
-    prevScrollHeight: 0,  // yOffset보다 이전에 위치한 섹션 높이 합
-    currentScene: 0,      // 현재 활성화된 scene
+    yOffset: 0,           // 현재 스크롤 값 설정
+    prevScrollHeight: 0,  // 현재 스크롤 값(yOffset)보다 이전에 위치한 섹션 높이의 합
+    currentScene: 0,      // 현재 활성화된 (보이는) 섹션
     enterNewScene: false, // 새로운 scene이 시작되는 순간 true
-    acc: 0.1,
-    delayedYOffset: 0,
-    rafId: null,
-    rafState: null,
+    acc: 0.1,             // 0.1 만큼 애니메이션 감속
+    delayedYOffset: 0,    // 지연된 yOffset
+    rafId: null,          // requestAnimationFrame 적용
+    rafState: null,       // requestAnimationFrame 상태
   });
   const [isSet, setIsSet] = useState(false);
 
@@ -29,6 +30,7 @@ const Main = ({sceneInfoList}) => {
     if (!isSet) {
       const sections = document.getElementsByTagName('section');
 
+      /** set data */
       sceneInfoList.map((sceneInfo, i) => {
         // set scene type & objs
         const section = sections[i];
@@ -48,7 +50,7 @@ const Main = ({sceneInfoList}) => {
         }
         sceneInfo.objs.imageElems = imageElems;
 
-        // set scene scrollHeight
+        // 각 섹션의 높이 설정
         switch (sceneInfo.sceneType) {
           case "sticky":
             sceneInfo.scrollHeight = HEIGHT_NUM * window.innerHeight;
@@ -63,19 +65,22 @@ const Main = ({sceneInfoList}) => {
         sceneInfo.objs.container.style.height = `${sceneInfo.scrollHeight}px`;
       });
 
-      // set layout
+      /** set layout */
+        // 페이지 중간에서 새로고침 했을 경우의 currentScene 설정
       let totalScrollHeight = 0;
       let currentSceneIndex = 0;
       for (let i = 0; i < sceneInfoList.length; i++) {
         totalScrollHeight += sceneInfoList[i].scrollHeight;
 
-        totalScrollHeight >= layoutData.yOffset
-          ? currentSceneIndex = i
-          : null
+        if (totalScrollHeight >= window.pageYOffset) {
+          currentSceneIndex = i;
+          break;
+        }
       }
 
       document.body.setAttribute('id', `show-scene-${currentSceneIndex}`);
 
+      // canvas 비율 설정 (height 100%, center 정렬)
       const heightRatio = window.innerHeight / 1080;
       sceneInfoList[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
       // sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
@@ -92,31 +97,77 @@ const Main = ({sceneInfoList}) => {
         }
       });
 
+      console.log(window.pageYOffset)
       setIsSet(true);
     }
   }, [isSet]);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    const scrollLoop = () => {
       setLayoutData(prev => {
         return {
           ...prev,
-          yOffset: window.pageYOffset
+          yOffset: window.yOffset,
         }
-      })
-    });
-    console.log(sceneInfos);
-    console.log(layoutData);
-  }, [sceneInfos]);
+      });
+      // let layout = {...layoutData};
+      //
+      // let enterNewScene = false;
+      // let prevScrollHeight = 0;
+      // for (let i = 0; i < layout.currentScene; i++) {
+      //   prevScrollHeight += sceneInfos[i].scrollHeight;
+      // }
+      // console.log(prevScrollHeight)
+      //
+      // // 정확한 계산을 위해 기존 yOffset 대신 delayedYOffset 으로 교체
+      // console.log("layout.currentScene", layout.currentScene)
+      // if (layout.delayedYOffset > prevScrollHeight + sceneInfos[layout.currentScene].scrollHeight) {
+      //   layout.enterNewScene = true;
+      //   layout.currentScene++;
+      //   // body의 id값에 따라 css에서 sticky-elem의 show / hide 설정
+      //   document.body.setAttribute('id', `show-scene-${currentScene}`);
+      // } else if (layoutData.delayedYOffset < prevScrollHeight) {
+      //   layout.enterNewScene = true;
+      //   if (layout.currentScene === 0) return;   // 브라우저 바운스 효과로 currentScene가 - 값이 되는 것을 방지
+      //   layout.currentScene--;
+      //   document.body.setAttribute('id', `show-scene-${currentScene}`);
+      // }
+      //
+      // // scene이 변경되는 순간은 palyAni 함수 실행 X (변경되는 순간 출력되는 음수값 때문에)
+      // if (enterNewScene) return;
+      //
+      // console.log("layoutData.currentScene =>", layout.currentScene)
+      // setLayoutData(prev => {
+      //   return {
+      //     ...prev,
+      //     yOffset: layout.yOffset,
+      //     prevScrollHeight: layout.prevScrollHeight,
+      //     currentScene: layout.currentScene,
+      //     enterNewScene: layout.enterNewScene,
+      //   }
+      // });
+
+      // playAni();
+    };
+
+    window.addEventListener('scroll', scrollLoop, {passive: true});
+    return () => window.removeEventListener('scroll', scrollLoop);
+  }, [sceneInfos, layoutData]);
+
+  useEffect(() => {
+    console.log(layoutData)
+
+  });
 
   return (
     <AppLayout>
-      <section className={styles.section1} data-scene-type='sticky'>
-        <h2 className={styles.title}>The Legend of &nbsp;Zelda<br/>Breath of the Wild</h2>
-        <div className={styles.sticky}>
-          <canvas id="vidio-canvas-0" width="1920" height="1080" className={styles.canvas}></canvas>
-        </div>
-      </section>
+      <StickySection/>
+      {/*<section className={styles.section1} data-scene-type='sticky'>*/}
+      {/*  <h2 className={styles.title}>The Legend of &nbsp;Zelda<br/>Breath of the Wild</h2>*/}
+      {/*  <div className={styles.sticky}>*/}
+      {/*    <canvas id="vidio-canvas-0" width="1920" height="1080" className={styles.canvas}></canvas>*/}
+      {/*  </div>*/}
+      {/*</section>*/}
       <section className={styles.section2} data-scene-type='normal'>
         <p className={styles.desc}>
           <strong className={styles.point}>Lorem</strong> ipsum dolor sit amet, <br/>consectetur adipiscing elit. <br/>Sed
@@ -124,7 +175,7 @@ const Main = ({sceneInfoList}) => {
         </p>
         <Gallery/>
       </section>
-      <section className={styles.section3} data-scene-type='normal'>
+      <section className={styles.section3} data-scene-type='sticky'>
         <h2 className={styles.title}>Calamity Ganon</h2>
         <img src="/images/video/001_intro/zelda 001.jpg" alt=""/>
       </section>
