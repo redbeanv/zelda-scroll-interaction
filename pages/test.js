7505907d35c823da0894532fd36d08bf967757d4
owopqowo <owo9370@gmail.com>
2022-02-03 15:27:40 +0900

    fix test.js

--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import styles from '../public/css/pages/test.module.css';
 
 export default function Canvas() {
@@ -7,9 +7,8 @@ export default function Canvas() {
   
   const imgArr = [];
   let yOffset = 0;
-  
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const wrap = wrapRef.current;
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
@@ -29,11 +28,26 @@ export default function Canvas() {
 
     wrap.style.height = `${window.innerHeight * 5}px`;
 
-    context.drawImage(imgArr[0], 0, 0, window.innerWidth, window.innerHeight);
+    let tempYOffset = window.pageYOffset;
+		let tempScrollCount = 0;
+		if (window.pageYOffset > 0) {
+			let siId = setInterval(() => {
+				window.scrollTo(0, tempYOffset);
+				tempYOffset += 5;
+				tempScrollCount++;
+				if(tempScrollCount > 20) {
+					clearInterval(siId);
+				}
+			}, 20);
+		} else {
+      console.log('test');
+      console.log(imgArr[0]);
+      context.drawImage(imgArr[0], 0, 0, window.innerWidth, window.innerHeight)
+    }
 
     window.addEventListener('scroll', () => {
       yOffset = window.pageYOffset;
-      const imgVal = [1, 357];
+      const imgVal = [0, 356];
       const num = Math.round(calc(imgVal, yOffset));
       context.drawImage(imgArr[num], 0, 0, window.innerWidth, window.innerHeight);
     });
@@ -41,7 +55,7 @@ export default function Canvas() {
 
   function setImg() {
     let img;
-    for (let i = 1; i < 357; i++) {
+    for (let i = 1; i <= 357; i++) {
       img = document.createElement('img');
       img.src = `/images/video/001_intro/zelda ${ i < 10 ? '00' + i : i < 100 ? '0' + i : i}.jpg`;
       imgArr.push(img);
